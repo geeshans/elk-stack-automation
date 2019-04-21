9bb175a8b87614010c959932f15b63ab53ea1b69
Geeshan Siriwardena <geeshans@gmail.com>
2019-04-21 19:39:17 +0530

    fix: make sure each node name is different

--- terraform/userdata-es.sh
+++ terraform/userdata-es.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 HOSTNAME=`curl  http://169.254.169.254/latest/meta-data/hostname`
+NODEID=`curl  http://169.254.169.254/latest/meta-data/instance-id`
 
 echo ES_JAVA_OPTS="\"-Xms1g -Xmx1g\"" >> /etc/sysconfig/elasticsearch
 echo MAX_LOCKED_MEMORY=unlimited >> /etc/sysconfig/elasticsearch
@@ -9,5 +10,6 @@ echo -e "y\n" | /usr/share/elasticsearch/bin/elasticsearch-plugin  install disco
 
 # Shortest configuration for Elasticsearch nodes to find each other
 echo "network.host: ${HOSTNAME}" >> /etc/elasticsearch/elasticsearch.yml
+echo "node.name: ${NODEID}" >> /etc/elasticsearch/elasticsearch.yml
 
 sudo service elasticsearch restart
