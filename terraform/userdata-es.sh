7a9f65d6debbfa069810d339848746aa351356a3
Geeshan Siriwardena <geeshans@gmail.com>
2019-04-21 18:52:32 +0530

    fix: fix elasticsearch memory

--- terraform/userdata-es.sh
+++ terraform/userdata-es.sh
@@ -5,10 +5,9 @@ echo ES_JAVA_OPTS="\"-Xms1g -Xmx1g\"" >> /etc/sysconfig/elasticsearch
 echo MAX_LOCKED_MEMORY=unlimited >> /etc/sysconfig/elasticsearch
 
 # Discovery EC2 plugin is used for the nodes to create the cluster in AWS
-echo -e "yes\n" | /usr/share/elasticsearch/bin/elasticsearch-plugin  install discovery-ec2
+echo -e "y\n" | /usr/share/elasticsearch/bin/elasticsearch-plugin  install discovery-ec2
 
 # Shortest configuration for Elasticsearch nodes to find each other
-echo "discovery.zen.hosts_provider: ec2" >> /etc/elasticsearch/elasticsearch.yml
 echo "network.host: ${HOSTNAME}" >> /etc/elasticsearch/elasticsearch.yml
 
 sudo service elasticsearch restart
